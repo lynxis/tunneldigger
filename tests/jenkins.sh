9e160b909c2a80b670ee496ff9d632705619d830
Alexander Couzens <lynxis@fe80.eu>
2016-02-22 13:30:05 +0100

    tests nosetest3 -s dont capture stdout

--- tests/jenkins.sh
+++ tests/jenkins.sh
@@ -16,20 +16,20 @@ cd $WORKSPACE/tests/
 # test the version aginst itself
 export CLIENT_REV=$NEW_REV
 export SERVER_REV=$NEW_REV
-nosetests3
+nosetests3 -s
 
 OLD_REV="c638231efca6b3a6e1c675ac0834a3e851ad1bdc 263eb59098fd11990b6ab75933fb7633055cbc9a 4e4f13cdc630c46909d47441093a5bdaffa0d67f"
 # do client NEW_REV against old revs
 for rev in $OLD_REV ; do
   export CLIENT_REV=$NEW_REV
   export SERVER_REV=$rev
-  nosetests3
+  nosetests3 -s
 done
 
 # do server NEW_REV against old revs
 for rev in $OLD_REV ; do
   export CLIENT_REV=$rev
   export SERVER_REV=$NEW_REV
-  nosetests3
+  nosetests3 -s
 done
 
